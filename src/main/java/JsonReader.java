b4de31adeae9239d0fe2823166e3720a426c3df1
ababup1192 <ababup1192@gmail.com>
2016-03-09 11:18:25 +0900

    Objects test

--- src/main/java/JsonReader.java
+++ src/main/java/JsonReader.java
@@ -1,5 +1,9 @@
 import com.google.gson.Gson;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 public class JsonReader {
@@ -12,10 +16,43 @@ public class JsonReader {
      * @param <T>          Javaオブジェクトの型
      * @return Optional[T]
      */
-    public static <T> Optional<T> readGson(String resourceName, Class<T> entityClass) {
+    public static <T> Optional<T> readJsonObject(String resourceName, Class<T> entityClass) {
         return FileReader.fileRead(resourceName).map(json -> {
             Gson gson = new Gson();
             return Optional.ofNullable(gson.fromJson(json, entityClass));
         }).orElse(Optional.empty());
+    }
+
+    public static <T> List<T> readJsonObjects(String resourceName, Class<T> entityClass) {
+        return FileReader.fileRead(resourceName).map(json ->
+                fromJsonList(json, entityClass))
+                .orElse(Collections.emptyList());
+    }
+
+    private static <T> List<T> fromJsonList(String json, Class<T> entityClass) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, new ListOfSomething<>(entityClass));
+    }
+
+    private static class ListOfSomething<X> implements ParameterizedType {
+
+        private Class<?> wrapped;
+
+        public ListOfSomething(Class<X> wrapped) {
+            this.wrapped = wrapped;
+        }
+
+        public Type[] getActualTypeArguments() {
+            return new Type[]{wrapped};
+        }
+
+        public Type getRawType() {
+            return List.class;
+        }
+
+        public Type getOwnerType() {
+            return null;
+        }
+
     }
 }
--- src/test/java/JsonReaderTest.java
+++ src/test/java/JsonReaderTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -9,8 +11,8 @@ import static org.junit.Assert.fail;
 public class JsonReaderTest {
 
     @Test
-    public void testJsonObjectRead() {
-        Optional<ObjectJson> objectJsonOpt = JsonReader.readGson("object.json", ObjectJson.class);
+    public void testReadJsonObject() {
+        Optional<ObjectJson> objectJsonOpt = JsonReader.readJsonObject("object.json", ObjectJson.class);
         objectJsonOpt.map(objectJson -> {
             assertThat(objectJson, is(new ObjectJson("word", 123, true)));
             return true;
@@ -18,6 +20,19 @@ public class JsonReaderTest {
             fail();
             return false;
         });
+    }
+
+    @Test
+    public void testReadJsonObjects() {
+        Collection<ObjectJson> objectJsonList = JsonReader.readJsonObjects("objects.json", ObjectJson.class);
+
+        assertThat(objectJsonList, is(Arrays.asList
+                (
+                        new ObjectJson("word", 123, true),
+                        new ObjectJson("word2", 456, false),
+                        new ObjectJson("word3", 789, true)
+                )
+        ));
     }
 
 }
